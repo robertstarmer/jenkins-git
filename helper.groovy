3ce0a40b6a428fb8662366759b62ce69aabc5a37
Mason B <hatef.ir@gmail.com>
2023-03-21 19:25:16 -0700

    Updated jenkins, this is a multiline commit message not sure if it goes through

--- helper.groovy
+++ helper.groovy
@@ -2,7 +2,10 @@ def get_property(String prop_file){
     echo "reading file $prop_file"
     prop = readProperties file:prop_file
     echo "returning prop.TEST_1: $prop.TEST_1"
-    String ret = $prop.TEST_1
+    String ret = "NOTHING"
+    if (prop?.TEST_1){
+        ret = "$prop.TEST_1"
+    }
 
     return ret
 }
