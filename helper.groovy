77f020656ac082b067923eb475954d837b57420b
Mason B <hatef.ir@gmail.com>
2023-03-21 19:19:37 -0700

    Updated jenkins

--- helper.groovy
+++ helper.groovy
@@ -2,7 +2,9 @@ def get_property(String prop_file){
     echo "reading file $prop_file"
     prop = readProperties file:prop_file
     echo "returning prop.TEST_1: $prop.TEST_1"
-    return $prop.TEST_1
+    String ret = $prop.TEST_1
+
+    return ret
 }
 
 return this
